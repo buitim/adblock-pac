b65f58afdd355bd13f66af42dedda77c276142cf
Timothy Bui <buitim@oregonstate.edu>
2020-01-19 17:34:17 -0800

    it's friday

--- easylist_pac.py
+++ easylist_pac.py
@@ -121,11 +121,12 @@ class EasyListPAC:
     def easylists_download_latest(self):
         osidList_url = 'https://dblmobile.oisd.nl/?.txt'
         easylist_url = 'https://easylist.to/easylist/easylist.txt'
-        easyprivacy_url = 'https://easylist.to/easylist/easyprivacy.txt'
-        fanboy_annoyance_url = 'https://easylist.to/easylist/fanboy-annoyance.txt'
-        fanboy_antifacebook = 'https://raw.githubusercontent.com/ryanbr/fanboy-adblock/master/fanboy-antifacebook.txt'
+        adguard_url = 'https://filters.adtidy.org/extension/ublock/filters/2_without_easylist.txt'
         antiAdBlockFilters = 'https://easylist-downloads.adblockplus.org/antiadblockfilters.txt'
-        self.download_list = [antiAdBlockFilters, easyprivacy_url, osidList_url] + self.extra_easylist_urls
+        # easyprivacy_url = 'https://easylist.to/easylist/easyprivacy.txt'
+        # fanboy_annoyance_url = 'https://easylist.to/easylist/fanboy-annoyance.txt'
+        # fanboy_antifacebook = 'https://raw.githubusercontent.com/ryanbr/fanboy-adblock/master/fanboy-antifacebook.txt'
+        self.download_list = [osidList_url, easylist_url, adguard_url, antiAdBlockFilters] + self.extra_easylist_urls
         # self.download_list = [fanboy_antifacebook, fanboy_annoyance_url, easyprivacy_url, easylist_url] + self.extra_easylist_urls
         self.file_list = []
         for url in self.download_list:
